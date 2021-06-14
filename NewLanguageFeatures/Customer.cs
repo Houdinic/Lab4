a8ef50145c42039ca84863bcc392778e8728734d
Marshall <363752922@qq.com>
2021-06-14 12:51:11 -0400

    Ex1

---
+++ NewLanguageFeatures/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewLanguageFeatures
+{
+    class Customer
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public int CustomerId { get;private set; }
+        public override string ToString()
+        {
+            return Name + "\t" + City;
+        }
+        public Customer(int Id)
+        {
+            CustomerId = Id;
+        }
+
+    }
+}
---
+++ NewLanguageFeatures/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NewLanguageFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c = new Customer(1);
+            c.Name = "Maria Anders";
+            c.City = "Berlin";
+            Console.WriteLine(c);
+        }
+    }
+}
