606f08973e61a007933d04bcaee8a044507603d5
Marshall <363752922@qq.com>
2021-06-14 15:37:19 -0400

    Ex2

--- NewLanguageFeatures/Program.cs
+++ NewLanguageFeatures/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace NewLanguageFeatures
 {
@@ -6,10 +8,28 @@ namespace NewLanguageFeatures
     {
         static void Main(string[] args)
         {
-            Customer c = new Customer(1);
-            c.Name = "Maria Anders";
-            c.City = "Berlin";
-            Console.WriteLine(c);
+            List<Customer> customers = CreateCustomers();
+
+            Console.WriteLine("Customers:\n");
+            foreach (Customer c in customers)
+                Console.WriteLine(c);
+
+
         }
+        static List<Customer> CreateCustomers()
+        {
+            return new List<Customer>
+    {
+        new Customer(1) { Name = "Maria Anders",     City = "Berlin"    },
+        new Customer(2) { Name = "Laurence Lebihan", City = "Marseille" },
+        new Customer(3) { Name = "Elizabeth Brown",  City = "London"    },
+        new Customer(4) { Name = "Ann Devon",        City = "London"    },
+        new Customer(5) { Name = "Paolo Accorti",    City = "Torino"    },
+        new Customer(6) { Name = "Fran Wilson",      City = "Portland"  },
+        new Customer(7) { Name = "Simon Crowther",   City = "London"    },
+        new Customer(8) { Name = "Liz Nixon",        City = "Portland"  }
+    };
+        }
+
     }
 }
