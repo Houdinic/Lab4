e4200105acdfc954cbbaf350d4f57152b114cf26
Marshall <363752922@qq.com>
2021-06-14 15:49:01 -0400

    Ex3

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace Exercise2
+namespace Exercise3
 {
     class Program
     {
+        static void VarTest()
+        {
+            var x = new [] { 1, 2, 3 };
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
         }
     }
 }
